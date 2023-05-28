7a907820c07e8bb9fae29b8ef794824e6b5c797a
Stanisław Skotniczny <stanislaw.skotniczny@microsoft.wsei.edu.pl>
2023-05-28 20:28:31 +0200

    .

--- OrderManager/Classes/OrderManagerContext.cs
+++ OrderManager/Classes/OrderManagerContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManager.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace OrderManager.Classes
 {
-    internal class OrderManagerContext
+    public class OrderManagerContext : DbContext
     {
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderItem> OrderItems { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entityTypeBuilder =>
+            {
+                entityTypeBuilder.ToTable("Customers");
+                entityTypeBuilder.HasKey(a => a.Id);
+                entityTypeBuilder.Property(a => a.Name);
+                entityTypeBuilder.Property(a => a.SecondName);
+                entityTypeBuilder.Property(a => a.Email);
+            });
+
+
+        }
+
+
+
+
     }
+
+   
+
+
 }
